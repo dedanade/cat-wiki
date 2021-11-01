1973d9534e1ad35b6b1db32045b92dbb6bf8f6ff
Daniel Adebonojo <dedan2343@gmail.com>
2021-11-01 11:23:52 +0100

    fixed "no unused car" error

--- src/Components/ViewBreed.jsx
+++ src/Components/ViewBreed.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
 import { GetBreed, GetBreedImages } from '../Services/ApiRequest';
 import Header from './Common/Header';
 import Footer from './Common/Footer';
